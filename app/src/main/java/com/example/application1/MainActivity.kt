c05406ea18d72b53799c3842e25c5cc0b1406ec1
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2025-12-19 17:31:00 +0100

    cd ..

--- app/src/main/java/com/example/application1/MainActivity.kt
+++ app/src/main/java/com/example/application1/MainActivity.kt
@@ -5,22 +5,28 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.application1.ui.theme.Application1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PersonInfo(
-                name = "Przemysław", surname = "Stokłosa"
-            )
+//            PersonInfo(
+//                name = "Przemysław", surname = "Stokłosa"
+//            )
+            matrix()
 
         }
     }
@@ -32,15 +38,30 @@ const val TAG = "Aplikacja"
 fun PersonInfo(name: String, surname: String) {
     Log.v(TAG, "Application info")
 
-    Text(
-        text = "$name $surname"
-    )
-    Text(text="Bielsko-Biała")
+    Column {
+        Row {
+            Text(
+                text = name, Modifier.padding(5.dp)
+            )
+            Text(
+                text = surname, Modifier.padding(5.dp)
+            )
+        }
 
+        Text(text = "Bielsko-Biała", Modifier.padding(5.dp))
+    }
+}
+
+@Composable
+fun matrix(){
+    var i = 10
+    Text(
+        text = "$i", Modifier.padding(2.dp)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-        PersonInfo("Przemysław", "Stokłosa")
+    PersonInfo("Przemysław", "Stokłosa")
 }

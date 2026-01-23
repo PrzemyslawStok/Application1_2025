7e5105853053f6b8ca3d7cb98dee71d1434925eb
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2026-01-23 16:53:00 +0100

    cd ..

---
+++ app/src/main/java/Game2.kt
@@ -0,0 +1,19 @@
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class Game2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Simple view")
+
+        }
+
+    }
+}

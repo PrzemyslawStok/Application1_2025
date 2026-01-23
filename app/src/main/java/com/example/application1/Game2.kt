b85bc2217dbed4c67cc7a0a8178f5505f303a92e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2026-01-23 17:43:24 +0100

    cd ..

--- app/src/main/java/com/example/application1/Game2.kt
+++ app/src/main/java/com/example/application1/Game2.kt
@@ -1,6 +1,7 @@
 package com.example.application1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -26,15 +27,18 @@ class Game2 : ComponentActivity() {
         var min = 0
         var max = 100
         val randomNumber = Random.nextInt(min, max)
+        val activity = this
         Column {
             Text("Zgadnij liczbę z przedziału 0-100 ($randomNumber)", fontSize = 25.sp)
             Text("Moja liczba: ${estimateNumber(min, max)}", fontSize = 30.sp)
             Row {
                 Button(onClick = {
+                    Toast.makeText(activity,"Coś jest nie tak...", Toast.LENGTH_LONG).show()
                 }) {
                     Text("Za mała")
                 }
                 Button(onClick = {
+                    Toast.makeText(activity,"Coś jest nie tak...", Toast.LENGTH_LONG).show()
                 }) {
                     Text("Za duża")
                 }

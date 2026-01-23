afafa23b70e17823f825795794fdeb2bfd000ea9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2026-01-23 16:44:34 +0100

    cd ..

--- app/src/main/java/com/example/application1/StartActivity.kt
+++ app/src/main/java/com/example/application1/StartActivity.kt
@@ -5,7 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -13,7 +19,17 @@ class StartActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Start Screen")
+            Column {
+                Text("Ekran startowy", fontSize = 35.sp)
+                Button(onClick = {
+                }) {
+                    Text("Gra 1")
+                }
+                Button(onClick = {
+                }) {
+                    Text("Gra 2")
+                }
+            }
 
         }
     }
